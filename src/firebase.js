68743e77826e92d4431402ac8ae8536eceecf092
Daniel Kim <kkdh1109@gmail.com>
2021-01-11 11:23:24 -0500

    app setup (i firebase-tools, node-sass)

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3N9kdZtGlnogxW28bZ3bxY4yUFm0iEpo",
+  authDomain: "clone-c9ef7.firebaseapp.com",
+  projectId: "clone-c9ef7",
+  storageBucket: "clone-c9ef7.appspot.com",
+  messagingSenderId: "976017757248",
+  appId: "1:976017757248:web:fa4d4545fbf349e5063ba5",
+  measurementId: "G-LE4N337543"
+};
