65e4703b3bffd7b9bad8818178790eb3f6c056aa
Daniel Kim <kkdh1109@gmail.com>
2021-01-11 11:34:42 -0500

    start header comp

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+    return (
+        <div className="header">
+            {/* <img alt="amazon logo" src= /> */}
+            <div className="header__searchBox">
+
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default Header;
