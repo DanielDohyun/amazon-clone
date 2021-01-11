1fd91c48e442f235497c3c7e97d98f0217529a50
Daniel Kim <kkdh1109@gmail.com>
2021-01-11 11:40:39 -0500

    add amazon logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.scss';
 
 function Header() {
     return (
         <div className="header">
-            {/* <img alt="amazon logo" src= /> */}
+            <Link to="/">
+                <img
+                    className="header__logo"
+                    alt="amazon logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                />
+            </Link>
+            
             <div className="header__searchBox">
 
             </div>
