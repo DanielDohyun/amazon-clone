1183d2e5bcc3745e6bfa4256f61b0292293c0993
Daniel Kim <kkdh1109@gmail.com>
2021-01-12 13:55:36 -0500

    create checkoutProduct comp and render inside of cehckout comp

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useStateValue } from '../../StateProvider';
+import CheckoutProduct from '../CheckoutProduct/CheckoutProduct';
 import './Checkout.scss';
 
 function Checkout() {
@@ -8,7 +9,33 @@ function Checkout() {
     return (
         <div className='checkout'>
             <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="ad img"
-                />
+            />
+            
+            {basket?.length === 0 ? (
+                <div>
+                    <h2>Your Shopping Basket is empty</h2>
+                    <p>
+                        You have no items in your basket. To buy one or more item, click "Add to basket" next to the item.
+                    </p>
+                </div>
+            ) : (
+                <div>
+                        <h2 className='checkout__title'>Your Shopping Basket</h2>
+                        {/* list out all of the checkout products  */}
+                        {
+                            basket.map(item => (
+                                <CheckoutProduct
+                                    id={item.id}
+                                    title={item.title}
+                                    price={item.price}
+                                    image={item.image}
+                                    rating={item.rating}
+                                />
+                            ))
+                        }
+
+                </div>
+            )}
             
         </div>
     )
---
+++ src/components/CheckoutProduct/CheckoutProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CheckoutProduct.scss';
+
+function CheckoutProduct() {
+    return (
+        <div className='checkoutProduct'>
+            
+        </div>
+    )
+}
+
+export default CheckoutProduct;
