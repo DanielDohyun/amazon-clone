8f45a02ee56bb47961f091f0ce0edec545fa4af6
Daniel Kim <kkdh1109@gmail.com>
2021-01-14 15:26:38 -0500

    create and start login comp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Checkout from './components/Checkout/Checkout';
+import Login from './components/Login/Login';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
             <Checkout />
           </Route>
           <Route path='login'>
-            {/* <Login /> */}
+            <Login />
           </Route>
           <Route path='/'>
             <Home />
