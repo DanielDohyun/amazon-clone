7329254f5bf35a3f140e17ccdbaa8c8508a18e04
Daniel Kim <kkdh1109@gmail.com>
2021-01-15 13:27:31 -0500

    i firebase, and create state to save email and password

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase';
 import './Login.scss';
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = e => {
+        e.preventDefault();
+
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then((auth) => {
+                //redirect to homepage
+            })
+            .catch(e => alert(e.message));
+        
+    };
+
+    const register = e => {
+        e.preventDefault();
+        
+        auth.createUserWithEmailAndPassword(email, password)
+            .then(auth => {
+            
+            })
+            .catch(e => alert(e.message));
+
+    };
+
     return (
         <div className='login'>
             <Link to="/">
@@ -17,10 +44,10 @@ function Login() {
                 <h1>Sign in</h1>
                 <form action="">
                     <h5>E-mail</h5>
-                    <input type="email"/>
+                    <input value={email} onChange={e => setEmail(e.target.value)} type="email"/>
                     <h5>Password</h5>
-                    <input type="password" />
-                    <button type='submit' className='login__signInButton'>Sign In</button>
+                    <input value={password} onChange={e => setPassword(e.target.value)} type="password" />
+                    <button onClick={login} type='submit' className='login__signInButton'>Sign In</button>
                 </form>
 
                 <p>
@@ -29,7 +56,7 @@ function Login() {
                 </p>
 
                 <Link to='/register'>
-                    <button className='login__registerButton'>Create your Amazon Account</button>
+                    <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
                 </Link>
 
             </div>
